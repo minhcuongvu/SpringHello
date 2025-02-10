1ea010b6fd206518557f9b10e7fa531dc958c707
Michael <noedigsti@gmail.com>
2025-02-10 16:54:58 +1000

    is it still building?

--- src/main/java/com/example/SpringHello/dto/LoginDto.java
+++ src/main/java/com/example/SpringHello/dto/LoginDto.java
@@ -12,4 +12,19 @@ import lombok.Setter;
 public class LoginDto {
   private String usernameOrEmail;
   private String password;
+  public String getUsernameOrEmail() {
+      return usernameOrEmail;
+  }
+
+  public void setUsernameOrEmail(String usernameOrEmail) {
+      this.usernameOrEmail = usernameOrEmail;
+  }
+
+  public String getPassword() {
+      return password;
+  }
+
+  public void setPassword(String password) {
+      this.password = password;
+  }
 }
